e077a39703fba7bb0efb9f338181c78928abb92e
unknown <royyanmz87@gmail.com>
2026-04-25 11:50:22 +0700

    logs

--- app/Models/StoreSetting.php
+++ app/Models/StoreSetting.php
@@ -16,9 +16,9 @@ class StoreSetting extends Model
         });
     }
 
-    public static function set(string $key, string $value): void
+    public static function set(string $key, ?string $value): void
     {
-        static::updateOrCreate(['key' => $key], ['value' => $value]);
+        static::updateOrCreate(['key' => $key], ['value' => $value ?? '']);
         Cache::forget("store_setting_{$key}");
     }
 }
